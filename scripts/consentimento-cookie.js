91cd34a72e7e988d80c24155e81a51e831670348
isaquegoncalves <114412444+isaquegoncalves@users.noreply.github.com>
2024-07-09 17:07:15 -0300

    Versão 1.9.3
    - Js introduzido no projeto por meio do banner de consentimento de cookies.
    
    - Adição do código Snippet do Adsense em todas as páginas do projeto.
    
    - Inserção de blocos de anúncios para testes nas seguintes páginas:
      - Black Ops 6
      - GTA VI
      - Resistência
      - Index.html
      - O Dilema das Redes
    
    - Integração de links em textos, os primeiros foram adicionados nas seções de termos e políticas.
    
    - As abas Termos, Políticas e Sobre foram revisados e readequados ao projeto.

---
+++ scripts/consentimento-cookie.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+    "palette": {
+    "popup": {
+        "background": "#060015"
+    },
+    "button": {
+        "background": "#3f00c0"
+    }
+    },
+    "theme": "classic",
+    "content": {
+    "message": "Este site usa cookies para garantir que você tenha a melhor experiência.",
+    "dismiss": "Entendi!",
+    "link": "Nossa Política de Cookies",
+    "href": "politicas-e-termos/politica-de-privacidade.html"
+    }
+});
+});
